7a7fabc91ac3d9a28871ea5d5e5e8757a09c4c72
Nadia Hansen <nadiahansen.kea@gmail.com>
2020-03-30 15:46:46 +0200

    Adding prococol, check and function

--- Client.py
+++ Client.py
@@ -1,4 +1,5 @@
 import socket
+
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 hostname = socket.gethostname()
@@ -7,35 +8,52 @@ print("Your Computer Name is:" + hostname)
 print("Your Computer IP Address is: " + IPAddr)
 server_address = ('localhost', 10000)
 counter = 0
-sent = sock.sendto(IPAddr.encode(), server_address)
-# Receive Connection
-data, server = sock.recvfrom(1024)
-print('C: Com-0 <', IPAddr, '> ')
-print('S: Com-0 {}'.format(data.decode()))
-if b'accept' in data:
-    sent = sock.sendto('accept < '.encode() + IPAddr.encode() + ' >'.encode(), server_address)
-    print('C: Com-0 accept <', IPAddr, '>')
-elif data:
-    sent = sock.sendto('denied < '.encode() + IPAddr.encode() + ' >'.encode(),server_address)
-    print('Connection Denied. Client closing')
-    sock.close()
-    exit()
 
-while True:
-    # Sending Message to Server
-    message = input('\nmsg:')
-    # Send data
-    print('C: msg-', counter, '= {!r}'.format(message))
-    sent = sock.sendto(message.encode(), server_address)
-    print('C: sending {} bytes to {}'.format(sent, server_address))
-    counter += 1
-    # Receive response
+
+# sending IP to server
+def handshake():
+    conn = 'com-' + str(counter) + '=' + IPAddr
+    sent = sock.sendto(conn.encode(), server_address)
+    # Receive Connection
     data, server = sock.recvfrom(1024)
-    print('S: res-', counter, '= {}'.format(data.decode()))
-    counter += 1
-    if message == 'exit':
-        try:
-            print('close')
-        finally:
-            sock.close()
-            exit()
+    dataresv = data.decode()
+    print('{}'.format(data.decode()))
+    data_spilt = data.decode().split('t ', 1)
+    # checking protocol and IP Address
+    if 'com-0 accept' in dataresv and socket.inet_aton(data_spilt[1]):
+        accept = 'com-' + str(counter) + ' accept'
+        # if OK sending accept to server
+        sent = sock.sendto(accept.encode(), server_address)
+        send()
+    elif data:
+        # if NOT OK closing
+        sent = sock.sendto('denied '.encode() + IPAddr.encode(), server_address)
+        print('Connection Denied. Client closing')
+        sock.close()
+        exit()
+
+
+def send():
+    count = 0
+    while True:
+        # Sending Message to Server
+        message = input('\nmsg:')
+        # Send data
+        sendmsg = 'msg-' + str(count) + ' ' + message
+        sent = sock.sendto(sendmsg.encode(), server_address)
+        print(sendmsg)
+        print('sending {} bytes to {}'.format(sent, server_address))
+        count += 1
+        # Receive response
+        data, server = sock.recvfrom(1024)
+        print('res-', count, '= {}'.format(data.decode()))
+        count += 1
+        if message == 'exit':
+            try:
+                print('close')
+            finally:
+                sock.close()
+                exit()
+
+
+handshake()
--- Server.py
+++ Server.py
@@ -1,4 +1,5 @@
 import socket
+
 # Create a UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Bind the socket to the port
@@ -8,41 +9,72 @@ sock.bind(server_address)
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
 
-# variable that holds the recieved data and the address is comming from
-data, address = sock.recvfrom(1024)
 
-# accepts or deny the access - if denied, close server.
-print('C: Com-0 < {} >'.format(data.decode()))
-ipdata = data.decode().split('.', 3)
-one, two, three, four = (int(ipdata[0])), (int(ipdata[1])), (int(ipdata[2])), (int(ipdata[3]))
-
-if (0 <= one <= 255) and (0 <= two <= 255) and (0 <= three <= 255) and (0 <= four <= 255):
-    sent = sock.sendto('accept < '.encode() + IPAddr.encode() + ' >'.encode(), address)
-    print('S: Com-0 accept', '<', IPAddr, '>')
-elif data:
-    try:
-        sent = sock.sendto('Connection Denied, closing server'.encode(), address)
-    finally:
-        sent = sock.sendto('Denied < '.encode() + IPAddr.encode() + ' >'.encode(), server_address)
-        print('Connection Denied. Server closing')
-        sock.close()
-        exit()
-
-data, address = sock.recvfrom(1024)
-print('C: Com-0 {} '.format(data.decode()))
-counter = 0
-while True:
+def handshake():
+    # variable that holds the recieved data and the address is coming from
     data, address = sock.recvfrom(1024)
-    print('C: msg-', counter, '=', data.decode())
-    print('S: = received {} bytes from {}'.format(len(data), address))
-    if data:
-        counter += 1
-        sent = sock.sendto('I am server'.encode(), address)
-        print('S: res-', counter, '= sent {} bytes back to {}'.format(sent, address))
-        counter += 1
-    if b'exit' in data:
+
+    # accepts or deny the access - if denied, close server.
+    print('{}'.format(data.decode()))
+    dataresv = data.decode()
+    data_spilt = data.decode().split('=', 1)
+    # checking the IP address and protocol
+    if "com-0" in dataresv and socket.inet_aton(data_spilt[1]):
+        # if OK sending accept to client
+        sent = sock.sendto('com-0 accept '.encode() + IPAddr.encode(), address)
+        print('com-0 accept=' + IPAddr)
+    elif data:
+        # if NOT OK closing server
         try:
-            print('close')
+            sent = sock.sendto('Connection Denied, closing server'.encode(), address)
+
         finally:
+            sent = sock.sendto('Denied '.encode() + IPAddr.encode(), server_address)
+            print('Connection Denied. Server closing')
             sock.close()
             exit()
+    # receiving accept from client and checking
+    data, address = sock.recvfrom(1024)
+    if 'com-0 accept' in data.decode():
+        print('ready')
+        first_msg()
+
+# checking first message is nr. 0 message
+def first_msg():
+    msg, address = sock.recvfrom(1024)
+    message = msg.decode()
+    if 'msg-0' in message:
+        send_msg(message, address)
+        msg_function()
+    else:
+        sent = sock.sendto('not protocol '.encode() + IPAddr.encode(), server_address)
+
+# send messages
+def send_msg(insert_msg, insert_address):
+    pre_counter = int(insert_msg[4])
+    counter = int(insert_msg[4])+1
+    # checking the count is i order
+    if counter - pre_counter == 1 and 'msg-' in insert_msg:
+        print(insert_msg)
+        print('received {} bytes from {}'.format(len(insert_msg), insert_address))
+        # respond client
+        sent = sock.sendto('I am server'.encode(), insert_address)
+        print('res-', counter, '= sent {} bytes back to {}'.format(sent, insert_address))
+
+
+def msg_function():
+    while True:
+        # read client message
+        msg, address = sock.recvfrom(1024)
+        # checking client message
+        send_msg(msg.decode(), address)
+        # if client sends exit, close socket.
+        if b'exit' in msg:
+            try:
+                print('close')
+            finally:
+                sock.close()
+                exit()
+
+
+handshake()
